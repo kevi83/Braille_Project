e151a454c1d6d0ffcea9bf0844f88d9054bb301f
connorAhearn <connor.ahearn305@gmail.com>
2018-01-26 21:21:21 -0500

    Printer Block and Braille Interpreter classes created
    -Connor

---
+++ Enamel/src/enamel/BrailleInterpreter.java
@@ -0,0 +1,42 @@
+package enamel;
+
+import java.util.HashMap;
+
+public class BrailleInterpreter {
+
+	public HashMap<Character, String> alphabet = new HashMap<Character, String>();
+	
+	public BrailleInterpreter() {
+		alphabet.put('a', "10000000");
+		alphabet.put('b', "11000000");
+		alphabet.put('c', "10100000");
+		alphabet.put('d', "10011000");
+		alphabet.put('e', "10001000");
+		alphabet.put('f', "11010000");
+		alphabet.put('g', "11011000");
+		alphabet.put('h', "11001000");
+		alphabet.put('i', "01010000");
+		alphabet.put('j', "01011000");
+		alphabet.put('k', "10100000");
+		alphabet.put('l', "11100000");
+		alphabet.put('m', "10110000");
+		alphabet.put('n', "10111000");
+		alphabet.put('o', "10101000");
+		alphabet.put('p', "11110000");
+		alphabet.put('q', "11111000");
+		alphabet.put('r', "11101000");
+		alphabet.put('s', "01110000");
+		alphabet.put('t', "01111000");
+		alphabet.put('u', "10100100");
+		alphabet.put('v', "11100100");
+		alphabet.put('w', "01011100");
+		alphabet.put('x', "10110100");
+		alphabet.put('y', "10111100");
+		alphabet.put('z', "10101100");
+		alphabet.put(' ', "11111111");
+	}
+	
+	public String getPins(char letter) {
+		return  alphabet.get(letter);
+	}
+}
---
+++ Enamel/src/enamel/Printer.java
@@ -0,0 +1,108 @@
+package enamel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Printer {
+	
+	File file;
+	FileOutputStream output;
+	ArrayList<String> lines = new ArrayList<>();
+	BrailleInterpreter interpreter = new BrailleInterpreter();
+	String[] buttonLabels = {"ONEE", "TWOO", "THREEE", "FOURR"};
+	
+	/**
+	 * 
+	 * @param fileName - Name of the file the scenario will be saved as
+	 * @param cells - Number of cells the machine used has <b>available</b>
+	 * @param buttonsAvailable - Number of buttons available on the machine
+	 * @throws IOException 
+	 */
+	public Printer(String fileName, int cells, int buttonsAvailable) throws IOException {
+		file = new File(fileName);
+		output = new FileOutputStream(file);
+		if(!file.exists()) file.createNewFile();
+		initialBlock(cells, buttonsAvailable);
+	}
+	
+	//Adds line starting with /~, string, ends with newline character
+	private void addConfig(String line) {
+		lines.add("/~" + line + "\n");
+	}
+	
+	//Adds line with string, ends with newline character
+	private void addSpoken(String line) {
+		lines.add(line + "\n");
+	}
+	
+	//Adds empty line (Like hitting enter)
+	private void newLine() {
+		lines.add("\n");
+	}
+	
+	//Inserts initial block to file declaring cells and buttons on machine
+	//buttonsAvailable refers to how many buttons are on the simulator / machine
+	private void initialBlock(int cells, int buttonsAvailable) {
+		addSpoken("Cell " + cells);
+		addSpoken("Button " + buttonsAvailable);
+		newLine();
+	}
+	
+	//Standard line used at the beginning of a block
+	private void clearPins() {
+		addConfig("disp-cell-clear:0");
+	}
+	
+	//Sets pins for the requested character
+	private void setPins(char letter) {
+		addConfig("disp-cell-pins:0 " + interpreter.getPins(letter));
+	}
+	
+	//Adds sound for the response depending on if the response is correct
+	private void addSound(boolean correct) {
+		if(correct) addConfig("correct.wav");
+		else addConfig("wrong.wav");
+	}
+	
+	//Skips to NEXTT, used if another block will follow the current block. used at the end
+	private void addSkip() {
+		addConfig("skip:NEXTT");
+	}
+	
+	//inputs NEXTT
+	private void addNext() {
+		addConfig("NEXTT");
+	}
+	
+	//Input declaring portion of a block
+	//NOTE: buttonsUsed refers to the buttons being used for the given scenario / block
+	private void addInputBlock(int buttonsUsed) {
+		for(int i = 0; i < buttonsUsed; i++) {
+			addConfig("skip-button:" + i + " " + buttonLabels[i]);
+		}
+		addConfig("user-input");
+	}
+	
+	//Spoken is the spoken response, button is the button that creates the response
+	//NOTE: button refers to the number displayed on the box / simulation. 1 = 1
+	private void addResponse(String spoken, int button, boolean correct) {
+		addConfig(buttonLabels[button-1]);
+		addSound(correct);
+		addSpoken(spoken);
+		addSkip();
+	}
+	
+	//Takes a block object as input and prints the contents accordingly
+	public void publishBlock(Block block) {
+		clearPins();
+		setPins(block.letter);
+		addSpoken(block.premise);
+		addInputBlock(block.buttonsUsed);
+		addResponse(block.correctResponse, 1, true);
+		addResponse(block.wrongResponse, 2, false);
+		addSkip();
+	}
+
+}
