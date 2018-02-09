2ab8522437d4a62fb57377e650146e6d9f0743e8
connorAhearn <connor.ahearn305@gmail.com>
2018-02-09 12:40:15 -0500

    More tests, more comments

---
+++ Enamel/src/tests/testBrailleInterpreter.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.*;
+import enamel.BrailleInterpreter;
+import static org.junit.Assert.*;
+
+public class testBrailleInterpreter {
+
+	@Test
+	public void testConstructor() {
+		BrailleInterpreter brailleInt = new BrailleInterpreter();
+	}
+	
+}
