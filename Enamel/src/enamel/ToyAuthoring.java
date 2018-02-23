72142bf90bd23673ebc8274c9352fd8f5d1d117b
MicahArndt <micaharndt@hotmail.com>
2018-02-23 10:45:42 -0500

    trying to push

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -4,6 +4,6 @@ public class ToyAuthoring {
 
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(false);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 2 + ".txt");
+    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
     }
 }
