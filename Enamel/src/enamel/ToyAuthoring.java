ab4fd3cfee596a7954e52cbebdf4e73b17a3c280
kevi83 <kevi83@my.yorku.ca>
2018-08-06 01:56:34 -0400

    // yeh toy authoring app works again!

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -3,7 +3,7 @@ package enamel;
 public class ToyAuthoring {
 
     public static void main(String[] args) { 	  	
-    	    ScenarioParser s = new ScenarioParser(false);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 2 + ".txt");
+    	    ScenarioParser s = new ScenarioParser(true);
+    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
     }
 }
